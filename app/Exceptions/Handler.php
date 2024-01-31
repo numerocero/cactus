241cb2816744479bf96f7bee19a3608c2779e5b2
Robert Lopez <robert.lopez.ve@gmail.com>
2024-01-31 17:32:59 -0300

    #main - disable redirection for api routes

--- app/Exceptions/Handler.php
+++ app/Exceptions/Handler.php
@@ -3,6 +3,7 @@
 namespace App\Exceptions;
 
 use Illuminate\Foundation\Exceptions\Handler as ExceptionHandler;
+use Illuminate\Http\Request;
 use Throwable;
 
 class Handler extends ExceptionHandler
@@ -25,6 +26,12 @@ class Handler extends ExceptionHandler
     {
         $this->reportable(function (Throwable $e) {
             //
+        });
+
+        $this->renderable(function(Throwable $exception, Request $request) {
+            if ($request->is('api/*')) {
+                $request->headers->set('Accept', 'application/json');
+            }
         });
     }
 }
