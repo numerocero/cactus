eefe97b4bf52ebd2d965cefffb838494655d97df
Robert Lopez <robert.lopez.ve@gmail.com>
2024-01-31 17:02:49 -0300

    #main - fix interactions migration

--- database/migrations/2024_01_31_180308_create_interactions_table.php
+++ database/migrations/2024_01_31_180308_create_interactions_table.php
@@ -12,7 +12,7 @@ return new class extends Migration
     public function up(): void
     {
         Schema::create('interactions', function (Blueprint $table) {
-            $table->uuid('id');
+            $table->uuid('id')->primary();
             $table->foreignUuid('customer_id')->constrained()->cascadeOnDelete()->cascadeOnUpdate();
             $table->string('description');
             $table->timestamps();
